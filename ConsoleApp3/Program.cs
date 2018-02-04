d45987b6786ae0d8540e8181389eeb420bd60d2b
Nikolay Moriakov <koly86@gmail.com>
2018-02-04 15:05:34 +0300

    add some futures at 04.02.2018

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+
+        //async and Task insted of void for methode var c = await TallyAsync(numbers);
+        static async Task Main(string[] args)
         {
             int[] numbers = {0b1, 0b10, 0b11, 0b100, 0b101, 0b110}; // 1) Convert to Binary
+           object[] numbersObject = { 0b1, 0b10, new object[]{0b11, 0b100, 0b101, 0b110} }; // 1) Convert to Binary
+
             var t = Tally(numbers);
             Console.WriteLine($"Sum1: {t.Item2} Sum2: {t.Item2} Sum:{t.Item3} bool:{t.Item3}" ); // 5) t.Item1
+
+            var c = await TallyAsync(numbers);
+
+            (int s, int y, bool e) = Tally(numbers); //14
+            var (b, _,_) = Tally(numbers); //15
 
             Console.WriteLine();
             Console.ReadKey();
@@ -26,6 +37,8 @@ namespace ConsoleApp3
             var r = (s: 0b0, c: 0b101); // 9) (s: 0, c: 1);
             var g = (values: values, r:r, x:0b110111);
 
+           
+
             var loop = (a:1, b:2, c:"df");
             foreach (var v in values)
             {
@@ -38,6 +51,84 @@ namespace ConsoleApp3
             // 10) return r  var r = (s: 0b0, c: 0b101); // 9) (s: 0, c: 1);
             // return values.Aggregate(r, (current, v) => (current.s + v, current.c + 0b1));
         }
+        //11 Task
+        private static async  Task<(int, int, bool)> TallyAsync(int[] values) 
+        {
+            return default;
+        }
+        //12 ValueTask //13 CancellationToken
+        private static async ValueTask<(int, int, bool)> TallyAsyncValueTask(int[] values, CancellationToken cancellationToken = default (CancellationToken)) //or default
+        {
+            return default;
+        }
+
+        private static (int, int) TallyLocalFunction(int[] values) 
+        {
+            var r = (s: 0b0, c: 0b101); 
+           
+            foreach (var v in values)
+            {
+                //13 local function
+                Add(v,1);
+            }
+            
+            return r;
+            void Add(int f, int c) => r = (r.s + f, r.c + c);
+            
+        }
+
+        private static (int, int) TallyObject(object[] values) 
+        {
+            var r = (s: 0b0, c: 0b101);
+
+            foreach (var v in values)
+            {
+                if (v is int i)
+                {
+                    //14 already checs if i is int
+                    Add(i,1);
+                }
+            }
+
+            return r;
+            void Add(int f, int c) => r = (r.s + f, r.c + c);
+
+        }
+
+        private static (int, int) TallyObjectSwitchCase(object[] values)
+        {
+            var r = (s: 0b0, c: 0b101);
+
+            foreach (var v in values)
+            {
+                
+                switch (v)
+                {
+                    case int i: 
+                        Add(i, 1); // 18 Shift + F12
+                        break;
+                    case object[] a when a.Length>0: //15 case condition
+                        var (sum, count) = TallyObjectSwitchCase(a);
+                        Add(sum,count);
+                        break;
+                    default: // 16 default can be anywhere
+                        throw new ArgumentException("Not numbers!", nameof(values));
+                    case object[] _:
+                    case null:
+                        break;
+                    case string s when int.TryParse(s, out var i): //17 OUT VAR declaration or int.TryParse(s, out var _):
+                        Add(i,1);
+                        break;
+                       
+
+                }
+            }
+
+            return r;
+            void Add(int f, int c) => r = (r.s + f, r.c + c);
+
+        }
+
     }
 
 }
